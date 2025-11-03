36618a959d76233bddeaf11dc301ae5675c00eb8
Diana Kozachenko <kozachenkodiana03@mail.ru>
2025-11-03 15:11:04 +0100

    Default values for colors in settings

--- src/app/components/options/options.component.ts
+++ src/app/components/options/options.component.ts
@@ -16,6 +16,12 @@ type FormGroupType = FormGroupTypeOptions & FormGroupTypeProductColors;
 
 type FormGroupValue = FormGroupValueOptions & FormGroupValueProductColors;
 
+const DEFAULT_COLORS_MAP: { [ ProductKey in ProductColorsKeys ]: string } = {
+  [ProductColorsKeys.COLOR_FOR_PRODUCT_IN_TIME]: 'green',
+  [ProductColorsKeys.COLOR_FOR_UNCERTAIN_PRODUCT]: 'orange',
+  [ProductColorsKeys.COLOR_FOR_PRODUCT_NOT_IN_TIME]: 'red',
+}
+
 @Component({
   selector: 'app-options',
   templateUrl: './options.component.html',
@@ -37,32 +43,70 @@ export class OptionsComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.patchFormByOptions();
+    this.subscribeOnHighlightOnSearchControlChange();
+    this.subscribeOnFormChanges();
+  }
 
+  private patchFormByOptions(): void {
+    forkJoin([
+      this.browserStorageService.getOptions(),
+      this.browserStorageService.getProductColors(),
+    ]).subscribe({
+      next: ([optionsValue, colorsValue]: [Partial<Options>, Partial<ProductColors>]) => {
+        if (optionsValue[OptionsKeys.HIGHLIGHT_DATES_ON_SEARCH_PAGE]) {
+          this.form.patchValue({
+            ...optionsValue,
+            ...colorsValue,
+          });
+        } else {
+          this.form.patchValue({
+            ...optionsValue,
+            [ProductColorsKeys.COLOR_FOR_PRODUCT_IN_TIME]: null,
+            [ProductColorsKeys.COLOR_FOR_UNCERTAIN_PRODUCT]: null,
+            [ProductColorsKeys.COLOR_FOR_PRODUCT_NOT_IN_TIME]: null,
+          });
+        }
+
+      },
+      error: (err) => {
+        console.error(`Ошибка при получении ключей '${OptionsKeys.HIGHLIGHT_DATES_ON_SEARCH_PAGE}', '${ProductColorsKeys.COLOR_FOR_PRODUCT_IN_TIME}',
+          '${ProductColorsKeys.COLOR_FOR_UNCERTAIN_PRODUCT}', '${ProductColorsKeys.COLOR_FOR_PRODUCT_NOT_IN_TIME}' из хранилища: ${err}`);
+      }
+    });
+  }
+
+  private subscribeOnHighlightOnSearchControlChange(): void {
     this.highlightOnSearchControlSubscription = this.form.controls[OptionsKeys.HIGHLIGHT_DATES_ON_SEARCH_PAGE]
       .valueChanges
       .pipe(startWith(false))
       .subscribe({
         next: (value: boolean | null) => {
-          // TODO: при вкл / выкл чекбокса эти значения могут заполнять прошлыми
-          // TODO: дефолтные значения
           if (value) {
             this.form.get(ProductColorsKeys.COLOR_FOR_PRODUCT_IN_TIME)?.enable();
             this.form.get(ProductColorsKeys.COLOR_FOR_UNCERTAIN_PRODUCT)?.enable();
             this.form.get(ProductColorsKeys.COLOR_FOR_PRODUCT_NOT_IN_TIME)?.enable();
+
+            this.form.patchValue({
+              [ProductColorsKeys.COLOR_FOR_PRODUCT_IN_TIME]: DEFAULT_COLORS_MAP[ProductColorsKeys.COLOR_FOR_PRODUCT_IN_TIME],
+              [ProductColorsKeys.COLOR_FOR_UNCERTAIN_PRODUCT]: DEFAULT_COLORS_MAP[ProductColorsKeys.COLOR_FOR_UNCERTAIN_PRODUCT],
+              [ProductColorsKeys.COLOR_FOR_PRODUCT_NOT_IN_TIME]: DEFAULT_COLORS_MAP[ProductColorsKeys.COLOR_FOR_PRODUCT_NOT_IN_TIME],
+            });
           } else {
             this.form.get(ProductColorsKeys.COLOR_FOR_PRODUCT_IN_TIME)?.disable();
             this.form.get(ProductColorsKeys.COLOR_FOR_UNCERTAIN_PRODUCT)?.disable();
             this.form.get(ProductColorsKeys.COLOR_FOR_PRODUCT_NOT_IN_TIME)?.disable();
 
             this.form.patchValue({
-              [ProductColorsKeys.COLOR_FOR_PRODUCT_IN_TIME]: '',
-              [ProductColorsKeys.COLOR_FOR_UNCERTAIN_PRODUCT]: '',
-              [ProductColorsKeys.COLOR_FOR_PRODUCT_NOT_IN_TIME]: '',
+              [ProductColorsKeys.COLOR_FOR_PRODUCT_IN_TIME]: null,
+              [ProductColorsKeys.COLOR_FOR_UNCERTAIN_PRODUCT]: null,
+              [ProductColorsKeys.COLOR_FOR_PRODUCT_NOT_IN_TIME]: null,
             });
           }
         }
-      })
+      });
+  }
 
+  private subscribeOnFormChanges(): void {
     this.formSubscription = this.form.valueChanges
       .pipe(
         debounceTime(500),
@@ -74,24 +118,6 @@ export class OptionsComponent implements OnInit, OnDestroy {
           console.error(`Ошибка при сохрании объекта ${this.form.value} в хранилище: ${err}`)
         }
       });
-  }
-
-  private patchFormByOptions(): void {
-    forkJoin([
-      this.browserStorageService.getOptions(),
-      this.browserStorageService.getProductColors(),
-    ]).subscribe({
-      next: ([optionsValue, colorsValue]: [Partial<Options>, Partial<ProductColors>]) => {
-        this.form.patchValue({
-          ...optionsValue,
-          ...colorsValue,
-        });
-      },
-      error: (err) => {
-        console.error(`Ошибка при получении ключей '${OptionsKeys.HIGHLIGHT_DATES_ON_SEARCH_PAGE}', '${ProductColorsKeys.COLOR_FOR_PRODUCT_IN_TIME}',
-          '${ProductColorsKeys.COLOR_FOR_UNCERTAIN_PRODUCT}', '${ProductColorsKeys.COLOR_FOR_PRODUCT_NOT_IN_TIME}' из хранилища: ${err}`);
-      }
-    });
   }
 
   ngOnDestroy(): void {
